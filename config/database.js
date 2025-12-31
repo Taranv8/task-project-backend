69c22e2a748c7548fc10fb8d1a6d688452082ce2
ts9559418 <ts9559418@gmail.com>
2025-12-31 21:08:21 +0000

    Fix login controller bug and update auth service

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://taran:taran@cluster0.jezra5c.mongodb.net/?appName=Cluster0', {
+
+    });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
