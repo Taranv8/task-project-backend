892297fe4cf33987b0f2ea438ff53e8d240a8a44
ts9559418 <ts9559418@gmail.com>
2025-12-31 20:36:25 +0000

    Fixed ESLint errors and removed functions folder

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-const app = require("../app"); // your existing Express app
-const connectDB = require("../config/database");
-
-// Connect to the database
-connectDB();
-
-// Export as Firebase Function
-exports.api = functions.https.onRequest(app);
